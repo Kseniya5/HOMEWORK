23d8486c8216893794f300bb5002a1b1b076c32e
kseniya <kseniyab555@gmail.com>
2016-06-07 16:30:37 +0200

    changes

---
+++ lesson7-8/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){   
+       PopUpHide();
+   });
+   function PopUpShow(){
+       $("#popup1").show();
+   }
+   function PopUpHide(){
+       $("#popup1").hide();
+   $(".wrapper").click(function() {
+       $("#popup1").hide();
+   
+   $(".wrapper").click(function(event) {
+       if {(event.currentTarget.id != 'popup1') {
+       $("#popup1").hide();}
+   }})
+   }
